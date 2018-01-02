9fae17707edc125f4a8cf2e688a29087b313b8cd
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-01-03 08:43:39 +0900

    adjust for APPVEYOR.

--- csharp/build.bat
+++ csharp/build.bat
@@ -1,11 +1,17 @@
 cd /d %~dp0
 
-set NUGET_EXE="C:\Program Files (x86)\NuGet\nuget.exe"
+if "%APPVEYOR%" == "True" (
+	set NUGET_EXE=nuget.exe
+	set EXTRA_CMD=/verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+) else (
+	set NUGET_EXE="C:\Program Files (x86)\NuGet\nuget.exe"
+	set EXTRA_CMD=
+)
 set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
 del /Q *.nupkg
 
-%MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Release  || (echo error && exit /b 1)
-%MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Debug    || (echo error && exit /b 1)
+%MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Release  %EXTRA_CMD% || (echo error && exit /b 1)
+%MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Debug    %EXTRA_CMD% || (echo error && exit /b 1)
 
 %NUGET_EXE% pack TimeStampTee.nuspec -Prop Configuration=Release
