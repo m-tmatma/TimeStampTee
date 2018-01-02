6b243b68a804a1983ec02bb16614119a7b6a9f31
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-01-02 14:20:19 +0900

    set Assembly information.

--- csharp/TimeStampTee/Properties/AssemblyInfo.cs
+++ csharp/TimeStampTee/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TimeStampTee")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("tee with Timestamp")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Masaru Tsuchiyama")]
 [assembly: AssemblyProduct("TimeStampTee")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Masaru Tsuchiyama 2018")]
+[assembly: AssemblyTrademark("Masaru Tsuchiyama")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
