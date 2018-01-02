c430bc3a51ec7b035707ec5868eba502d240436c
Masaru Tsuchiyama <m.tmatma@gmail.com>
2018-01-03 08:48:03 +0900

    fix remove file command.

--- csharp/build.bat
+++ csharp/build.bat
@@ -9,9 +9,11 @@ if "%APPVEYOR%" == "True" (
 )
 set MSBUILD_EXE="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-del /Q *.nupkg
+del /Q *.nupkg || echo OK
 
-%MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Release  %EXTRA_CMD% || (echo error && exit /b 1)
-%MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Debug    %EXTRA_CMD% || (echo error && exit /b 1)
+echo %MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Release  %EXTRA_CMD% 
+     %MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Release  %EXTRA_CMD% || (echo error && exit /b 1)
+echo %MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Debug    %EXTRA_CMD% || (echo error && exit /b 1)
+     %MSBUILD_EXE% TimeStampTee.sln /p:Configuration=Debug    %EXTRA_CMD% || (echo error && exit /b 1)
 
 %NUGET_EXE% pack TimeStampTee.nuspec -Prop Configuration=Release
